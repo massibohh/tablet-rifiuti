654bcb21c937bd331548f4550615dba41e4fc0d9
massibohh <massibohhdeveloper@gmail.com>
2026-08-31 00:54:20 +0200

    Create data.js

---
+++ data.js
@@ -0,0 +1,9 @@
+const wasteCalendar = {
+  0: ["🟤 VETRO", "🟢 ORGANICO"],
+  1: ["🟡 PLASTICA", "⚪ METALLO"],
+  2: ["🟢 ORGANICO"],
+  3: ["🔵 CARTA", "📦 CARTONE"],
+  4: ["🟢 ORGANICO"],
+  5: ["⚫ RESIDUO NON RICICLABILE"],
+  6: []
+};
